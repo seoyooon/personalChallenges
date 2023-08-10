e0fa9d6287bf0924262121456482a1741061dd2e
seoyooon <seoyooon95@naver.com>
2023-08-10 16:39:49 +0900

    자바 별찍기 기초

---
+++ stars.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class stars {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
